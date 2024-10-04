3ca332388e936ca0e552ad0505e386aa2eeaee04
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-10-04 20:46:52 +0000

    cambio

--- src/app.py
+++ src/app.py
@@ -4,17 +4,12 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Habilitar CORS para todas las rutas
+CORS(app)
 
-# Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///starwars.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from models import *  # Importar modelos
-from routes import *  # Importar rutas
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from routes import *  # Import routes
